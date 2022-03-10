90f05bbb9475087964603f1069a06df41eb0d155
cordel22 <cordelfenevall@gmail.com>
2022-03-11 00:50:14 +0100

    first commit 2hrs in

---
+++ src/MainContent.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+export default class MainContent
+extends Component {
+constructor(props) {
+  super(props);
+  this.state = { 
+    pageTitle: "Customers", 
+    customersCount: 5,
+    customers: [
+      { id: 1, name: "Scott", phone: "123-456", address: {city: "Edinburgh"}},
+      { id: 2, name: "Jim", phone: "789-101", address: {city: "IndianaPolis"}},
+      { id: 3, name: "Spock", phone: "121-314", address: {city: "VulcansVille"}},
+      { id: 4, name: "J-L", phone: "151-617", address: {city: "Toulouse"}},
+      { id: 5, name: "Deanna", phone: "181-920", address: {city: "Miami"}}
+    ] };
+}
+  render() {
+    return (
+      <div><h4 className="border-bottom m-1 p-1">
+        {this.state.pageTitle}
+        <br />
+        <span className="badge badge-secondary m-2">
+          { this.state.customersCount}
+        </span>
+
+        <button classNme="btn btn-info" onClick={this.onRefreshClick}>
+          Refresh
+        </button>
+        </h4>
+        
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Customer Name</th>
+              <th>Phone</th>
+              <th>City</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.customers.map((cust) => {
+                return (
+                  <tr key={cust.id}>
+              <td>{cust.id}</td>
+              <td>{cust.name}</td>
+              <td>{cust.phone}</td>
+              <td>{cust.address.city}</td>
+            </tr>
+                )
+              })
+            }
+            
+          </tbody>
+          </table>
+
+
+        </div>
+    );
+  }
+
+  //  Executes when the user clicks on Refresh button
+  onRefreshClick = () => {
+    console.log("Refresh clicked")
+    this.setState({
+      customersCount: 7
+    });
+  }
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar/* { NavBar } */ from "./NavBar.jsx"/* /index.js */;
+import App from './App.jsx';
+import "./index.css";
+//import App from './App';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  
+  <App />,
+  document.getElementById('root'),
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* 
+<NavBar />
+<React.StrictMode>
+    <App />
+  </React.StrictMode>
+ */
+///* var element = <button class="btn btn-danger">Hello motherland</button>; */
