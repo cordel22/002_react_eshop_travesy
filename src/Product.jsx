53d157d8ad1cd88d86990315fb9e159e45a166e1
cordel22 <cordelfenevall@gmail.com>
2022-03-11 22:17:03 +0100

    props children 3:12

--- src/Product.jsx
+++ src/Product.jsx
@@ -15,17 +15,17 @@ export default class Product extends Component {
       <div class="col-lg-6">
         <div className="card m-2">
       <div className="card-body">
-      {/* <div className="text-muted"># {this.props.id}</div> */}
-      {/* <div className="text-muted"># {this.state.id}</div> */}
       <div className="text-muted"># {this.state.product.id}</div>
 
-      {/* <h5 className="pt-5 border-top">{this.props.productName}</h5> */}
-      {/* <h5 className="pt-2 border-top">{this.state.productName}</h5> */}
       <h5 className="pt-2 border-top">{this.state.product.productName}</h5>
 
-      {/* <div>${this.props.price}</div> */}
-      {/* <div>${this.state.price}</div> */}
+      
       <div>${this.state.product.price}</div>
+      </div>
+      {/* card body ends here */}
+
+      <div className="card-footer text-right">
+        {this.props.children}
       </div>
       
       </div>  
