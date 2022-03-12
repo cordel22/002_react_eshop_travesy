e001eaa768727f4d2a2e1ef49360435d4b3cc600
cordel22 <cordelfenevall@gmail.com>
2022-03-13 00:49:12 +0100

    basic routing 5:35

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar.jsx";
 //  import MainContent from "./MainContent";
-//  import ShoppingCart from "./ShoppingCart.jsx";
+ import ShoppingCart from "./ShoppingCart.jsx";
 import Login from "./Login.jsx";
+import Dashboard from "./Dashbord.jsx";
+import CustomersList from "./CustomersList";
+//import { Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//  import { Routes } from "react-router";
+
+import ReactDOM from "react-dom";
+
 
 
 export default class App extends Component {
   render() {
     return (
-          <React.Fragment>
-          <NavBar />
-          <Login></Login>
-          {/* <ShoppingCart></ShoppingCart> */}
-          </React.Fragment>
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Login />}/>
+              <Route path="/dashboard" element={<Dashboard />}/>
+              <Route path="/customers" element={<CustomersList />}/>
+              <Route path="/cart" element={<ShoppingCart />}/>
+            </Routes>
+          
+          </BrowserRouter>
+         
     );
   }
 }
+
+{/* <React.Fragment> */}
+          {/* <Login></Login> */}
+          {/* <ShoppingCart></ShoppingCart> */} 
+          {/* </React.Fragment> */}
+          {/* <Dashboard></Dashboard> */}
---
+++ src/Dashbord.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Dashboad</h4>
+      </div>
+    )
+  }
+}
